121ea1b848d76694f74213227162e74e049fba6e
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-07-23 23:47:21 +0530

    lab 3
    Code of Ans 1

--- A1.cpp
+++ A1.cpp
@@ -1,40 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *x, int *y)
-{
-    int t = *x;
-    *x = *y;
-    *y = t;
-}
-
-void sort(int *a, int n)
-{
-    int i, j, min_idx;
-
-    for (i = 0; i < n - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (a[j] < a[min_idx])
-                min_idx = j;
-
-        swap(&a[min_idx], &a[i]);
-    }
-}
-
 int main()
 {
-    int n;
+    int n, ans = 0;
     cin >> n;
     int *a = new int[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
+        ans = ans ^ a[i];
     }
-    sort(a, n);
-    cout << "Second smallest:" << a[1] << endl;
-    cout << "Second largest:" << a[n - 2] << endl;
+    cout << ans;
     delete[] a;
     return 0;
 }
