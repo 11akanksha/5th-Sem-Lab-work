8b6831f0736ef23abe168866b524c35042412714
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-07-23 23:47:57 +0530

    lab 3
    Code of ans 2

--- A2.cpp
+++ A2.cpp
@@ -1,40 +1,58 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-void swap(int *x, int *y)
+//1st approach:
+bool prime1(int n)
 {
-    int t = *x;
-    *x = *y;
-    *y = t;
+    if (n <= 1)
+        return false;
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+//2nd approach:
+int prime2(int n)
+{
+    int c;
+    for (int i = 2; i <= n / 2; ++i)
+    {
+        if (n % i == 0)
+        {
+            c = 1;
+            break;
+        }
+    }
+    return c;
+}
+
+// 3rd approach:
+int prime3(int n)
+{
+    int c;
+    for (int i = 2; i <= sqrt(n); ++i)
+    {
+        if (n % i == 0)
+        {
+            c = 1;
+            break;
+        }
+    }
+    return c;
 }
 
 int main()
 {
     int n;
     cin >> n;
-    int *a = new int[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    int s = n;
-    if (n % 2 != 0)
-    {
-        int s = n - 1;
-    }
-    for (int i = 0; i < s / 2; i++)
-    {
-        swap(a[i], a[(s - 1) - i]);
-    }
-    for (int i = 0; i < s; i += 2)
-    {
-        swap(a[i], a[i + 1]);
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+    cout << "By 1st approach:\t";
+    prime1(n) ? cout << "a prime no" << endl : cout << "not a prime" << endl;
+    cout << "By 2nd approach:\t";
+    prime2(n) == 1 ? cout << "not a prime" << endl : cout << "a prime no" << endl;
+    cout << "By 3rd approach:\t";
+    prime3(n) == 1 ? cout << "not a prime" << endl : cout << "a prime no" << endl;
     return 0;
 }
