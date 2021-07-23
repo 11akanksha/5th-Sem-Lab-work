15ef8fdb68bb68ccf7cf4c7909b8a8f61adeb41f
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-07-23 23:48:29 +0530

    lab 3
    code of ans 3

--- A3.cpp
+++ A3.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 using namespace std;
 
+void swap(int *x, int *y)
+{
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+void sort_arr(int *a, int n)
+{
+    int i, j, min_idx;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (a[j] < a[min_idx])
+                min_idx = j;
+
+        swap(&a[min_idx], &a[i]);
+    }
+}
+
 int main()
 {
-    int n;
+    int n, min_diff = INT_MAX;
     cin >> n;
     int *a = new int[n];
     for (int i = 0; i < n; i++)
-    {
         cin >> a[i];
-    }
-    int p = 1;
-    for (int i = 0; i < n; i++)
+    sort_arr(a, n);
+    for (int i = 1; i < n; i++)
     {
-        p *= a[i];
+        int diff = a[i] - a[i - 1];
+        if (diff < min_diff)
+            min_diff = diff;
     }
-    for (int i = 0; i < n; i++)
-    {
-        a[i] = p / a[i];
-    }
-    //the result:
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+    cout << min_diff;
+    delete[] a;
     return 0;
 }
