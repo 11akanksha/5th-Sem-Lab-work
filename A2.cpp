d9ca077c32750c7a7f215c93a5a3188df5c696d4
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-07-31 22:26:58 +0530

    Lab 4
    The 2nd ques

--- A2.cpp
+++ A2.cpp
@@ -1,58 +1,41 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
-
-//1st approach:
-bool prime1(int n)
-{
-    if (n <= 1)
-        return false;
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-
-    return true;
-}
-
-//2nd approach:
-int prime2(int n)
-{
-    int c;
-    for (int i = 2; i <= n / 2; ++i)
-    {
-        if (n % i == 0)
-        {
-            c = 1;
-            break;
-        }
-    }
-    return c;
-}
-
-// 3rd approach:
-int prime3(int n)
-{
-    int c;
-    for (int i = 2; i <= sqrt(n); ++i)
-    {
-        if (n % i == 0)
-        {
-            c = 1;
-            break;
-        }
-    }
-    return c;
-}
 
 int main()
 {
-    int n;
+    int n, m = 0;
     cin >> n;
-    cout << "By 1st approach:\t";
-    prime1(n) ? cout << "a prime no" << endl : cout << "not a prime" << endl;
-    cout << "By 2nd approach:\t";
-    prime2(n) == 1 ? cout << "not a prime" << endl : cout << "a prime no" << endl;
-    cout << "By 3rd approach:\t";
-    prime3(n) == 1 ? cout << "not a prime" << endl : cout << "a prime no" << endl;
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] > m)
+            m = a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == m)
+        {
+            a[i] = -1;
+            continue;
+        }
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] < a[j])
+            {
+                a[i] = a[j];
+                break;
+            }
+        }
+    }
+    a[n - 1] = -1;
+    //display the results:
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    delete[] a;
     return 0;
 }
