e48dd152436b2da33237dc305d711ace52c79f5c
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-01 22:41:13 +0530

    lab 2
    ans 2

--- A2.cpp
+++ A2.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void swap(int *x, int *y)
+{
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
 int main()
 {
-    int n, m = 0;
+    int n;
     cin >> n;
     int *a = new int[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        if (a[i] > m)
-            m = a[i];
     }
-    for (int i = 0; i < n; i++)
+    int s = n;
+    if (n % 2 != 0)
     {
-        if (a[i] == m)
-        {
-            a[i] = -1;
-            continue;
-        }
-
-        for (int j = i + 1; j < n; j++)
-        {
-            if (a[i] < a[j])
-            {
-                a[i] = a[j];
-                break;
-            }
-        }
+        int s = n - 1;
     }
-    a[n - 1] = -1;
-    //display the results:
+    for (int i = 0; i < s / 2; i++)
+    {
+        swap(a[i], a[(s - 1) - i]);
+    }
+    for (int i = 0; i < s; i += 2)
+    {
+        swap(a[i], a[i + 1]);
+    }
+
     for (int i = 0; i < n; i++)
     {
         cout << a[i] << " ";
     }
     cout << endl;
-    delete[] a;
     return 0;
 }
