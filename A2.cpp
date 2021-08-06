913238a14f2b93aa337131289861a704f1152dbf
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-06 13:42:15 +0530

    Lab 5
    2nd ques

--- A2.cpp
+++ A2.cpp
@@ -1,58 +1,28 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
-//1st approach:
-bool prime1(int n)
+void solve(int n)
 {
-    if (n <= 1)
-        return false;
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-
-    return true;
-}
-
-//2nd approach:
-int prime2(int n)
-{
-    int c;
-    for (int i = 2; i <= n / 2; ++i)
+    if (n == 1)
     {
-        if (n % i == 0)
-        {
-            c = 1;
-            break;
-        }
+        cout << "we reached 1" << endl;
+        return;
     }
-    return c;
-}
-
-// 3rd approach:
-int prime3(int n)
-{
-    int c;
-    for (int i = 2; i <= sqrt(n); ++i)
+    for (int i = 0; i < n; i++)
     {
-        if (n % i == 0)
+        for (int j = 1; j <= n; j++)
         {
-            c = 1;
-            break;
+            cout << j << " ";
         }
+        cout << endl;
     }
-    return c;
+    solve(n - 2);
 }
 
 int main()
 {
     int n;
     cin >> n;
-    cout << "By 1st approach:\t";
-    prime1(n) ? cout << "a prime no" << endl : cout << "not a prime" << endl;
-    cout << "By 2nd approach:\t";
-    prime2(n) == 1 ? cout << "not a prime" << endl : cout << "a prime no" << endl;
-    cout << "By 3rd approach:\t";
-    prime3(n) == 1 ? cout << "not a prime" << endl : cout << "a prime no" << endl;
+    solve(n);
     return 0;
 }
