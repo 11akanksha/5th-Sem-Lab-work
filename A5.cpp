ed00e5bdc0be30659d5d5ffd28a24e0d14262af1
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-08 14:37:56 +0530

    Lab 5
    5th ques

--- A5.cpp
+++ A5.cpp
@@ -1,32 +1,63 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void BubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+    }
+}
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
 
 int main()
 {
-    int n;
+    int n, k;
+    cout << "No of elements: ";
     cin >> n;
+    cout << "Fill the array: " << endl;
     int *a = new int[n];
-    int m = 0; //max element;
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        if (a[i] > m)
-        {
-            m = a[i];
-        }
     }
-    int *f = new int[m];
+    BubbleSort(a, n);
+    cout << "Using Bubble sort: \n";
     for (int i = 0; i < n; i++)
-    {
-        f[a[i]]++;
-    }
-    for (int i = 0; i < m; i++)
-    {
-        if (f[i] % 2 == 1)
-        {
-            cout << i << " ";
-        }
-    }
+        cout << a[i] << " ";
+    cout << endl;
+    insertionSort(a, n);
+    cout << "Using Insertion sort: \n";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
     cout << endl;
     return 0;
 }
