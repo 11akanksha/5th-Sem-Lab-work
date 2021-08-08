db1093fd69cba92bbaae30a61f18d236d125360b
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-08 14:37:32 +0530

    Lab 5
    4th ques

--- A4.cpp
+++ A4.cpp
@@ -1,76 +1,40 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int non_zero(int a[4][4])
+int binarySearch(int arr[], int l, int r, int x)
 {
-    int s = 16;
-    for (int i = 0; i < 4; i++)
+    while (l <= r)
     {
-        for (int j = 0; j < 4; j++)
-        {
-            if (a[i][j] == 0)
-            {
-                s--;
-            }
-        }
+        int m = l + (r - l) / 2;
+
+        if (arr[m] == x)
+            return m;
+
+        if (arr[m] < x)
+            l = m + 1;
+
+        else
+            r = m - 1;
     }
-    return s;
+
+    return -1;
 }
 
-int sum_above_diag(int a[4][4])
+int main(void)
 {
-    int sum = 0;
-    for (int i = 0; i < 4; i++)
+    int n, k;
+    cout << "No of elements: ";
+    cin >> n;
+    cout << "Fill the array: " << endl;
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 4; j++)
-        {
-            if (j > i)
-            {
-                sum += a[i][j];
-            }
-        }
+        cin >> a[i];
     }
-    return sum;
-}
-
-int sum_below_diag(int a[4][4])
-{
-    int sum = 0;
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (i > j)
-            {
-                sum += a[i][j];
-            }
-        }
-    }
-    return sum;
-}
-
-int prod_diag(int a[4][4])
-{
-    int p = 1;
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (i == j)
-            {
-                p *= a[i][j];
-            }
-        }
-    }
-    return p;
-}
-
-int main()
-{
-    int a[4][4] = {{1, 2, 3, 4}, {5, 6, 0, 8}, {0, 10, 11, 12}, {13, 0, 15, 16}};
-    cout << "Non zero: " << non_zero(a) << endl;
-    cout << "sum above diagonal: " << sum_above_diag(a) << endl;
-    cout << "sum below diagonal: " << sum_below_diag(a) << endl;
-    cout << "product of diagonal: " << prod_diag(a) << endl;
+    cout << "Enter the element you want to search for:" << endl;
+    cin >> k;
+    int result = binarySearch(a, 0, n - 1, k);
+    (result == -1) ? cout << "Element is not present in array" : cout << "Element is present at index " << result;
+    delete[] a;
     return 0;
 }
