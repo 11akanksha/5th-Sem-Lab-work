1c6374a40aa10cd63e7219b23ad6be542fcefd15
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-13 13:37:00 +0530

    Lab 6
    answer 2

--- A2.cpp
+++ A2.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void solve(int n)
-{
-    if (n == 1)
-    {
-        cout << "we reached 1" << endl;
-        return;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-    solve(n - 2);
-}
-
 int main()
 {
     int n;
+    cout << "No of elements : " << endl;
     cin >> n;
-    solve(n);
+    int *a = new int(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int max = a[0], min = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+        else
+        {
+            min = a[i];
+        }
+    }
+    cout << "Max : " << max << "\t Min: " << min << endl;
+    delete[] a;
     return 0;
 }
