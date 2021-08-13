c32ee311a215425de3dde15b0bf86a9159a315c0
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-13 13:36:32 +0530

    Lab 6
    Ans 1

--- A1.cpp
+++ A1.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void solve(int n)
+//if n==1 T(n)= O(1)
+//else 2T(n/2)+2n.
+
+void func(int n)
 {
     if (n == 1)
     {
         cout << n << endl;
         return;
     }
-    for (int i = 1; i <= n; i++)
+    else
     {
-        cout << i << " ";
+        for (int i = 1; i <= n; i++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+
+        for (int i = n; i > 0; i--)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+        cout << "------------------------" << endl;
     }
-    cout << endl;
-    solve(n - 1);
+    func(n / 2);
+    func(n / 2);
 }
 
 int main()
 {
     int n;
+    cout << "Value of n:\t";
     cin >> n;
-    solve(n);
+    func(n);
     return 0;
 }
