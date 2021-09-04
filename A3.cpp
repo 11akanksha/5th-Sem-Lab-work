2c944862a11e90501b09d6348fb85b755cc3cbd3
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-09-04 19:51:41 +0530

    Lab 8

--- A3.cpp
+++ A3.cpp
@@ -1,77 +1,94 @@
 #include <iostream>
-#include <climits>
+#include <string.h>
 using namespace std;
 
-void findMinAndMax(int arr[], int low, int high, int &min, int &max)
+void build_max_heap(int *a, int i, int s)
+{
+    int p = (i - 1) / 2;
+
+    if (p >= 0 && a[i] > a[p])
+    {
+        int t = a[p];
+        a[p] = a[i];
+        a[i] = t;
+        build_max_heap(a, (i - 1) / 2, s);
+    }
+}
+
+void build_min_heap(int *a, int i, int s)
 {
 
-    if (low == high)
+    int p = (i - 1) / 2;
+
+    if (p >= 0 && a[i] < a[p])
     {
-        if (max < arr[low])
-        {
-            max = arr[low];
-        }
-
-        if (min > arr[high])
-        {
-            min = arr[high];
-        }
-
-        return;
+        int t = a[p];
+        a[p] = a[i];
+        a[i] = t;
+        build_min_heap(a, (i - 1) / 2, s);
     }
+}
 
-    if (high - low == 1)
+string MaxMin(int *h, int n)
+{
+    string s = "MIN";
+    if (h[0] > h[1] && h[0] > h[2])
     {
-        if (arr[low] < arr[high])
+        s = "MAX"; //heap was a max heap
+        for (int i = 0; i < n; i++)
         {
-            if (min > arr[low])
-            {
-                min = arr[low];
-            }
-
-            if (max < arr[high])
-            {
-                max = arr[high];
-            }
+            build_min_heap(h, i, n);
         }
-        else
-        {
-            if (min > arr[high])
-            {
-                min = arr[high];
-            }
-
-            if (max < arr[low])
-            {
-                max = arr[low];
-            }
-        }
-        return;
     }
+    return s;
+}
 
-    int mid = (low + high) / 2;
+string MinMax(int *h, int n)
+{
+    string s = "MAX";
+    if (h[0] < h[1] && h[0] < h[2])
+    {
+        s = "MIN"; //heap was a min heap
+        for (int i = 0; i < n; i++)
+        {
+            build_max_heap(h, i, n);
+        }
+    }
+    return s;
+}
 
-    findMinAndMax(arr, low, mid, min, max);
-
-    findMinAndMax(arr, mid + 1, high, min, max);
+void print_arr(int *a, int n)
+{
+    cout << "The array is : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
+    int *a = new int[20];
     int n;
-    cout << "No of elements : " << endl;
+    cout << "Enter size of heap: ";
     cin >> n;
-    int *arr = new int(n);
+    cout << "Enter elements:\n";
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        cin >> a[i];
     }
-
-    int max = INT_MIN, min = INT_MAX;
-
-    findMinAndMax(arr, 0, n - 1, min, max);
-
-    cout << "Max : " << max << "\t Min: " << min << endl;
-    delete[] arr;
+    if (MaxMin(a, n) == "MAX")
+    {
+        cout << "Your heap was a max heap.Its now converted to min heap:\n";
+        print_arr(a, n);
+    }
+    else
+    {
+        MinMax(a, n);
+        cout << "Your heap was a min heap.Its now converted to max heap:\n";
+        print_arr(a, n);
+    }
+    delete[] a;
     return 0;
 }
