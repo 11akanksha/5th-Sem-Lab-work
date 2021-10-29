a038cf51baa9d7130642e687254a7b35e381caa9
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-10-29 20:01:32 +0530

    lab 13 ans 2

--- A2.cpp
+++ A2.cpp
@@ -1,62 +1,40 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
-struct Activity
+void addEdge(vector<vector<int>> &aM, int u, int v, int wt)
 {
-    int start, finish, actNo;
-};
-
-bool cmp(Activity a1, Activity a2)
-{
-    return (a1.finish < a2.finish);
-}
-
-void Activity_Selection(Activity a[], int n)
-{
-    sort(a, a + n, cmp);
-    cout << "Activity Selected:\n";
-    int i = 0;
-    cout << "{" << a[i].actNo;
-    for (int j = 1; j < n; j++)
-    {
-        if (a[j].start >= a[i].finish)
-        {
-            cout << " , " << a[j].actNo;
-            i = j;
-        }
-    }
-    cout << "}" << endl;
+    aM[u][v] = wt;
+    aM[v][u] = wt;
 }
 
 int main()
 {
     int n;
-    cout << "Enter total no of activities: ";
+    cout << "Enter the no of nodes of the graph:\n";
     cin >> n;
-    cout << "Enter activity no. of each activity:" << endl;
-    Activity *A = new Activity[n];
+    vector<vector<int>> adjM(n, vector<int>(n, 0));
+    cout << "Enter the weighted edges:" << endl;
+    int a, b, w;
+    bool add_more = true;
+
+    do
+    {
+        cin >> a >> b >> w;
+        addEdge(adjM, a, b, w);
+
+        cout << "Continue?\t";
+        cin >> add_more;
+    } while (add_more);
+
+    cout << "Adjacency Matrix:" << endl;
     for (int i = 0; i < n; i++)
     {
-        int no;
-        cin >> no;
-        A[i].actNo = no;
+        for (int j = 0; j < n; j++)
+        {
+            cout << adjM[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << "Enter start time of each activity:" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        int s;
-        cin >> s;
-        A[i].start = s;
-    }
-    cout << "Enter end time of each activity:" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        int f;
-        cin >> f;
-        A[i].finish = f;
-    }
-    Activity_Selection(A, n);
-    delete[] A;
     return 0;
 }
